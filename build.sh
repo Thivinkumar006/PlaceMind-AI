cc5fdcf80c997183326bcb2c5cc05cbf0a3bac8e
Thivinkumar006 <thivinkumar006@gmail.com>
2026-08-30 22:32:49 +0530

    Update build.sh to symlink backend/app

--- build.sh
+++ build.sh
@@ -5,3 +5,9 @@ set -o errexit
 cd backend
 pip install -r requirements.txt
 alembic upgrade head
+cd ..
+
+# Remove the hacky app wrapper and replace it with a symlink to backend/app
+# This ensures that `uvicorn app.main:app` works flawlessly on Render!
+rm -rf app
+ln -s backend/app app
